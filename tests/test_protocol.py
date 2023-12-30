0b38c7353b9a17ec10148322b3e553669ed64b39
Brian Donelan <briandonelan@gmail.com>
2023-12-30 18:13:43 -0500

    Parameterize test_read_frame_simple_error cases

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -12,7 +12,10 @@ def test_read_frame_simple_string(buffer, expected):
     actual = extract_frame_from_buffer(buffer)
     assert actual == expected
 
-def test_read_frame_simple_error():
-    buffer = b"-Error message\r\n"
+@pytest.mark.parametrize("buffer, expected", [
+    (b"-Par", (None, 0)),
+    (b"-Error message\r\n", (SimpleError("Error message"), 16))
+])
+def test_read_frame_simple_error(buffer, expected):
     actual = extract_frame_from_buffer(buffer)
-    assert actual == (SimpleError("Error message"), 16)
+    assert actual == expected
